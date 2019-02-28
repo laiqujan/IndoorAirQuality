28ee4a497fc045b718338911a8f97c70089dbaa1
Muhammad Laiq <ujankhan66@gmail.com>
2019-02-28 23:07:15 +0200

    created initial dashboard and sensor UI

--- app/src/main/java/com/example/indoorairquality/MainActivity.java
+++ app/src/main/java/com/example/indoorairquality/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.indoorairquality;
 
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static android.view.View.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button dashBoard = findViewById(R.id.dashBoard);
+        Button sensor = findViewById(R.id.sensor);
+        dashBoard.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),DashBoard.class);
+                startActivity(i);
+            }
+        });
+        sensor.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),AppSensor.class);
+                startActivity(i);
+            }
+        });
     }
+
 }
